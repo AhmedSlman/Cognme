8563b4bc96ee9cd26062aeab459684092f9a00af
AhmedSlman <ahmedslman151@gmail.com>
2024-02-24 18:56:15 +0200

    f

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,8 @@
+import 'package:cognme/features/auth/presentation/views/sign_up_view.dart';
 import 'package:cognme/features/home/presetation/view/home_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/auth/presentation/views/sign_in_view.dart';
 import '../../features/on_boarding/presentation/views/on_bording_view.dart';
 import '../../features/splash/presentation/views/splash_view.dart';
 
@@ -13,6 +15,14 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: "/onBoarding",
       builder: (context, state) => const OnBoardingView(),
+    ),
+    GoRoute(
+      path: '/signUp',
+      builder: (context, state) => const SignUpView(),
+    ),
+    GoRoute(
+      path: '/signIn',
+      builder: (context, state) => const SignInView(),
     ),
     GoRoute(
       path: '/homeView',

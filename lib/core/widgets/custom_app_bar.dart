7f60fc3c51f3c928fc912ca353d4930aba955032
ali <140660389+alielsharqawy@users.noreply.github.com>
2024-02-26 16:43:09 +0200

    cutomAppBar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -3,20 +3,22 @@ import 'package:cognme/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.label, this.appBarIcon});
+  const CustomAppBar({super.key, required this.label, this.appBarLeading,this.appBarTrailing});
   final String label;
-  final Widget? appBarIcon;
+  final Widget? appBarLeading;
+  final Widget? appBarTrailing;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 20
     ),
       child: ListTile(
-        leading: const Icon(Icons.arrow_back),
-        title: Text(label,style: AppStyles.s24.copyWith(
+        leading: appBarLeading,
+        title: Text(label,style: AppStyles.s28.copyWith(
           color: AppColors.black,
           fontWeight: FontWeight.bold
         ),),
+        trailing: appBarTrailing,
       ),
     );
   }
